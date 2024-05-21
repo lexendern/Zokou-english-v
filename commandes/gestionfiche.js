d34c35de50796791e1cde4496fe0249ca35f8098
My lord <lordeiser@gmail.com>
2024-05-22 00:12:27 +0100

    Update gestionfiche.js

--- commandes/gestionfiche.js
+++ commandes/gestionfiche.js
@@ -792,7 +792,7 @@ zokou(
 zokou(
     {
         nomCom: 'jaxnemesis',
-        categorie: ‘Seabox'
+        categorie: 'Seabox'
     },
     async (dest, zk, commandeOptions) => {
         const { repondre, arg, ms } = commandeOptions;
