6197c68f33a0eda60c1f26a482b17e402866966f
My lord <lordeiser@gmail.com>
2024-05-25 11:24:12 +0100

    Update gmapseabox.js

--- commandes/gmapseabox.js
+++ commandes/gmapseabox.js
@@ -24,3 +24,80 @@ zokou(
         }
     }
 );
+
+
+zokou(
+    {
+        nomCom: 'spawnlootlake',
+        categorie: '🔹SEABOX🔹'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0)  {
+            const lien = 'https://telegra.ph/file/f1cbad9de1f51fbd56c29.jpg';
+            const msg = `░░░━━━━━━━━━
+🔷𝕃𝕆ℂ𝔸𝕃 𝕊ℙ𝔸𝕎ℕ 𝔹𝔸𝕋𝕋𝕃𝔼 𝔹𝕆𝕏🌐
+             ░░░━━━━━━━━━━━
+*🔹Localisation*: _LOOT LAKE_
+    ░░━━━
+*🔹name*: *Lust lucci*
+- *desc*: _Une box 📦 contenant une arme est située à 2km_
+            ░░░━━━━━━━━━━
+░░░━━━━━━━━`;
+            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+   
+        }
+    }
+);
+
+
+zokou(
+    {
+        nomCom: 'spawnriskymns',
+        categorie: '🔹SEABOX🔹'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0)  {
+            const lien = 'https://telegra.ph/file/e458f44d5b9f70838a13f.jpg';
+            const msg = `░░░━━━━━━━━━
+🔷𝕃𝕆ℂ𝔸𝕃 𝕊ℙ𝔸𝕎ℕ 𝔹𝔸𝕋𝕋𝕃𝔼 𝔹𝕆𝕏🌐
+             ░░░━━━━━━━━━━━
+*🔹Localisation*: _RISKY MNS_
+    ░░━━━
+*🔹name*: *Jonh smith*
+- *desc*: _Une box 📦 contenant une arme est située à 2km_
+            ░░░━━━━━━━━━━
+░░░━━━━━━━━`;
+            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+   
+        }
+    }
+);
+
+zokou(
+    {
+        nomCom: 'spawnlazylinks',
+        categorie: '🔹SEABOX🔹'
+    },
+    async (dest, zk, commandeOptions) => {
+        const { repondre, arg, ms } = commandeOptions;
+
+        if (!arg || arg.length === 0)  {
+            const lien = 'https://telegra.ph/file/cdffd0f45f7fbed1a8a9d.jpg';
+            const msg = `░░░━━━━━━━━━
+🔷𝕃𝕆ℂ𝔸𝕃 𝕊ℙ𝔸𝕎ℕ 𝔹𝔸𝕋𝕋𝕃𝔼 𝔹𝕆𝕏🌐
+             ░░░━━━━━━━━━━━
+*🔹Localisation*: _LAZY LINKS_
+    ░░━━━
+*🔹name*: *Yūkunn*
+- *desc*: _Une box 📦 contenant une arme est située à 2km_
+            ░░░━━━━━━━━━━
+░░░━━━━━━━━`;
+            zk.sendMessage(dest, { image: { url: lien }, caption: msg }, { quoted: ms });
+   
+        }
+    }
+);
